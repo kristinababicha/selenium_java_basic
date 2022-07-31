7e181fc6b654ebca095768f63839338164855f82
Janis Dzalbe <dzalbe.janis@accenture.com>
2022-07-31 18:12:00 +0300

    Update Tasks

--- src/selenium/sample/Sample1Task.java
+++ src/selenium/sample/Sample1Task.java
@@ -14,9 +14,17 @@ public class Sample1Task {
     public void goToHomepage() throws Exception {
 //        TODO:
 //         define driver
+
+        System.setProperty("webdriver.chrome.driver", libWithDriversLocation + "chromedriver" + new selenium.ChangeToFileExtension().extension());
+         WebDriver driver =new ChromeDriver();
+         driver.get("https://kristinek.github.io/site/index2.html");
 //         go to https://kristinek.github.io/site/index2.html
 //         get title of page
+        System.out.println(driver.getTitle());
 //         get URL of current page
+        System.out.println(driver.getCurrentUrl());
 //         close browser
+        Thread.sleep(5000);
+        driver.quit();
     }
 }
