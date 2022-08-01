dd4e4cfa678967baebd40f192c0232f6a4d2959c
ahmed <ahmed2050.mahmoud@gmail.com>
2022-08-01 14:29:28 +0200

    Task2

--- src/selenium/sample/Sample2Task.java
+++ src/selenium/sample/Sample2Task.java
@@ -7,6 +7,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.pagefactory.ByAll;
 
 import java.io.File;
 import java.util.List;
@@ -30,6 +31,7 @@ public class Sample2Task {
     // method which is being run after each test
     @After
     public void endingTests() throws Exception {
+
         driver.quit();
     }
 
@@ -37,25 +39,37 @@ public class Sample2Task {
     public void findElementByID() throws Exception {
 //         TODO:
 //         get text "Heading 2 text" using id
+        
+        System.out.println(driver.findElement(By.id("heading_2")).getText());
     }
 
     @Test
     public void findElementByName() throws Exception {
 //         TODO:
 //         get attribute "id" and "value" of button "This is also a button" using name
+        WebElement elm=driver.findElement(By.name("randomButton2"));
+        System.out.println(elm.getAttribute("Id"));
+        System.out.println(elm.getAttribute("value"));
     }
 
     @Test
     public void findElementByClassFirst() throws Exception {
 //         TODO:
 //         get first text of class "test" (should be "Test Text 1")
+        System.out.println(driver.findElement(By.className("text")).getText());;
     }
 
     @Test
     public void findElementByClassAll() throws Exception {
 //         TODO:
-//         get size text of class "test" (should be 5)
+//         get size text of class "test" (should be 5
 //         get text of class "test"
 //         get third text of class "test" (should be "Test Text 4")
+        List<WebElement> elems = driver.findElements(By.className("test"));
+        System.out.println(elems.size());
+        elems.forEach(WebElement -> {
+            System.out.println(WebElement.getText());
+        });
+        System.out.println(elems.get(2). getText());
     }
 }
