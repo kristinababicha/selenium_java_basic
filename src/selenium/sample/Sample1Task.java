fc272ab3e399acbb45da7b11ca1abcd3c0c5e356
sona104@gmail.com <alexeystr@yahoo.co.uk>
2022-08-01 12:12:48 +0300

    Task 1

--- src/selenium/sample/Sample1Task.java
+++ src/selenium/sample/Sample1Task.java
@@ -2,6 +2,7 @@ package selenium.sample;
 
 
 import org.junit.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
@@ -18,5 +19,22 @@ public class Sample1Task {
 //         get title of page
 //         get URL of current page
 //         close browser
+//        define driver
+        System.setProperty("webdriver.chrome.driver", libWithDriversLocation + "chromedriver" + new selenium.ChangeToFileExtension().extension());
+        WebDriver driver = new  ChromeDriver();
+        driver.get ("https://kristinek.github.io/site/index2.html");
+        //open test homepage
+        driver.get("https://google.com");
+        driver.get("https://kristinek.github.io/site/");
+        System.out.println(driver.findElement(By.id("h1")).getText());
+        //get title of page
+        System.out.println(driver.getTitle());
+        //get URL of current page
+        System.out.println(driver.getCurrentUrl());
+        //Sleep for 10 seconds
+        Thread.sleep(10000);
+
+        //Close browser
+        driver.quit();}
     }
-}
+
