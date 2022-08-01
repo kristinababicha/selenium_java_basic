cf9509af830124fd71800bae5f6261ef8d136035
SreeViggy <viki.vconnect@gmail.com>
2022-08-01 11:44:06 +0300

    SampleTask1-Vignesh

--- src/selenium/sample/Sample1Task.java
+++ src/selenium/sample/Sample1Task.java
@@ -1,6 +1,7 @@
 package selenium.sample;
 
 
+import org.junit.BeforeClass;
 import org.junit.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -10,13 +11,25 @@ import java.io.File;
 public class Sample1Task {
     static String libWithDriversLocation = System.getProperty("user.dir") + File.separator + "lib" + File.separator;
 
+    @BeforeClass
+    public static void before() {
+        System.setProperty("webdriver.chrome.driver",libWithDriversLocation+"chromedriver");
+    }
+
     @Test
     public void goToHomepage() throws Exception {
 //        TODO:
 //         define driver
+        WebDriver driver = new ChromeDriver();
+
 //         go to https://kristinek.github.io/site/index2.html
+        driver.get("https://kristinek.github.io/site/index2.html");
+
 //         get title of page
+        System.out.println("Title of the page is "+driver.getTitle());
 //         get URL of current page
+        System.out.println("URL of the current page is "+driver.getCurrentUrl());
 //         close browser
+        driver.close();
     }
 }
