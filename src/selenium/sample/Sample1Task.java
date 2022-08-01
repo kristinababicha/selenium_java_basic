7c6f7ce43027cd02879347ffff176c99b3a11575
Indra Metra <indrametra@gmail.com>
2022-08-01 12:11:03 +0300

    Sample1Task

--- src/selenium/sample/Sample1Task.java
+++ src/selenium/sample/Sample1Task.java
@@ -6,6 +6,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 import java.io.File;
+import java.sql.Driver;
 
 public class Sample1Task {
     static String libWithDriversLocation = System.getProperty("user.dir") + File.separator + "lib" + File.separator;
@@ -14,9 +15,16 @@ public class Sample1Task {
     public void goToHomepage() throws Exception {
 //        TODO:
 //         define driver
+        System.setProperty("webdriver.chrome.driver", libWithDriversLocation + "chromedriver" + new selenium.ChangeToFileExtension().extension());
+            WebDriver driver = new ChromeDriver ();
 //         go to https://kristinek.github.io/site/index2.html
+         driver.get("https://kristinek.github.io/site/index2.html");
 //         get title of page
+            System.out.println(driver.getTitle());
 //         get URL of current page
+        System.out.println(libWithDriversLocation);
 //         close browser
+        driver.quit();
+
     }
 }
