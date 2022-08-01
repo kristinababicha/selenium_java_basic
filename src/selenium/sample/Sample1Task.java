84cf45b2be5a4057dc919db875c747d6dc246336
Tina Shumilova <shutiva@gmail.com>
2022-08-01 12:03:47 +0300

    Sample1Task finished

--- src/selenium/sample/Sample1Task.java
+++ src/selenium/sample/Sample1Task.java
@@ -1,6 +1,5 @@
 package selenium.sample;
 
-
 import org.junit.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -8,15 +7,23 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import java.io.File;
 
 public class Sample1Task {
+
     static String libWithDriversLocation = System.getProperty("user.dir") + File.separator + "lib" + File.separator;
 
     @Test
     public void goToHomepage() throws Exception {
-//        TODO:
+
+//        TODO
 //         define driver
+        System.setProperty("webdriver.chrome.driver", libWithDriversLocation + "chromedriver" + new selenium.ChangeToFileExtension().extension());
+        WebDriver newDriver = new ChromeDriver();
 //         go to https://kristinek.github.io/site/index2.html
+        newDriver.get("https://kristinek.github.io/site/index2.html");
 //         get title of page
+        System.out.println(newDriver.getTitle());
 //         get URL of current page
+        System.out.println(newDriver.getCurrentUrl());
 //         close browser
+        newDriver.quit();
     }
 }
