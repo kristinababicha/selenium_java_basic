5ec28747e999680e860f1b6b0ef2c78ace9eafde
Neringa Šleževičienė <faulttester@gmail.com>
2022-08-01 11:34:22 +0300

    First task without exception

--- src/selenium/sample/Sample1Task.java
+++ src/selenium/sample/Sample1Task.java
@@ -11,12 +11,20 @@ public class Sample1Task {
     static String libWithDriversLocation = System.getProperty("user.dir") + File.separator + "lib" + File.separator;
 
     @Test
-    public void goToHomepage() throws Exception {
+    public void goToHomepage() {
 //        TODO:
 //         define driver
 //         go to https://kristinek.github.io/site/index2.html
 //         get title of page
 //         get URL of current page
 //         close browser
+        System.setProperty("webdriver.chrome.driver", libWithDriversLocation + "chromedriver" + new selenium.ChangeToFileExtension().extension());
+        ChromeDriver dr = new ChromeDriver();
+        dr.get("https://kristinek.github.io/site/index2.html");
+
+        System.out.println(dr.getTitle());
+        System.out.println(dr.getCurrentUrl());
+        dr.close();
+
     }
 }
