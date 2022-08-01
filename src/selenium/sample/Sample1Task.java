67d52cd69d32ddc03f932153607ffd88f57c576d
vmambetova <vitamambetova@gmail.com>
2022-08-01 11:53:46 +0300

    Sample1Task Vita Mambetova

--- src/selenium/sample/Sample1Task.java
+++ src/selenium/sample/Sample1Task.java
@@ -14,9 +14,16 @@ public class Sample1Task {
     public void goToHomepage() throws Exception {
 //        TODO:
 //         define driver
+        // does not work without line nr 18
+        System.setProperty("webdriver.chrome.driver", libWithDriversLocation + "chromedriver" + new selenium.ChangeToFileExtension().extension());
+        WebDriver webDriver = new ChromeDriver();
 //         go to https://kristinek.github.io/site/index2.html
+        webDriver.get("https://kristinek.github.io/site/");
 //         get title of page
+        System.out.println(webDriver.getTitle());
 //         get URL of current page
+        System.out.println(webDriver.getCurrentUrl());
 //         close browser
+        webDriver.quit();
     }
 }
