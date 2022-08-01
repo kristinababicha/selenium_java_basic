ed44d913e89cdbc78da325836a3619726c3fbb97
ahmed <ahmed2050.mahmoud@gmail.com>
2022-08-01 11:13:28 +0200

    Update Tasks

--- src/selenium/sample/Sample1Task.java
+++ src/selenium/sample/Sample1Task.java
@@ -11,14 +11,16 @@ public class Sample1Task {
     static String libWithDriversLocation = System.getProperty("user.dir") + File.separator + "lib" + File.separator;
 
     @Test
+
     public void goToHomepage() throws Exception {
 //        TODO:
 //         define driver
 
         System.setProperty("webdriver.chrome.driver", libWithDriversLocation + "chromedriver" + new selenium.ChangeToFileExtension().extension());
          WebDriver driver =new ChromeDriver();
+        //         go to https://kristinek.github.io/site/index2.html
          driver.get("https://kristinek.github.io/site/index2.html");
-//         go to https://kristinek.github.io/site/index2.html
+
 //         get title of page
         System.out.println(driver.getTitle());
 //         get URL of current page
