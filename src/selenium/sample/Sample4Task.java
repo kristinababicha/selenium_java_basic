203236da47ad3b8e3a8b6a65edd5816c037cf926
Kristine Ringe <109912680+Christeen1@users.noreply.github.com>
2022-08-01 19:32:20 +0300

    Task 4

--- src/selenium/sample/Sample4Task.java
+++ src/selenium/sample/Sample4Task.java
@@ -37,23 +37,42 @@ public class Sample4Task {
     @Test
     public void enterNumber() throws Exception {
 //         TODO:
+
+        String inputNumber = "18";
 //        enter a number under "Number"
+        driver.findElement(By.id("number")).clear();
+        driver.findElement(By.id("number")).sendKeys("inputNumber");
+
 //        check that button is not clickable "Clear Result"
+assertFalse(driver.findElement(By.id("clear_result_button_number")).isEnabled());
 //        check that text is not displayed
+       assertFalse(driver.findElement(By.id("result_number")).isDisplayed());
 //        click on "Result" button
+        driver.findElement(By.id("result_button_number")).click();
 //        check that text is displayed
+        assertTrue(driver.findElement(By.id("result_number")).isDisplayed());
 //        check that the correct Text appears ("You entered number: "NUMBER YOU ENTERED"")
-//        check that the button "Clear Result" is clickable now
+        assertEquals("You entered number: \"" + inputNumber + "\"",
+        driver.findElement(By.id("result_number")).getText());
+        //        check that the button "Clear Result" is clickable now
+        assertTrue(driver.findElement(By.id("clear_result_button_number")).isEnabled());
 //        click on "Clear Result"
+        driver.findElement(By.id("clear_result_button_number")).click();
 //        check that the text is now (""), but it is not displayed
+    assertFalse(driver.findElement(By.id("result_number")).isDisplayed());
     }
 
     @Test
     public void clickOnLink() throws Exception {
 //         TODO:
 //        check current url is base_url
+        assertEquals(driver.getCurrentUrl(),base_url);
 //        click on "This is a link to Homepage"
+        driver.findElement(By.id("homepage_link")).click();
 //        check that current url is not base_url
+        assertNotEquals(driver.getCurrentUrl(),base_url);
 //        verify that current url is homepage
+        assertEquals("https://kristinek.github.io/site/", driver.getCurrentUrl());
+
     }
 }
