873ff465bc9f91ef219e273b606cb158e48f661d
gizemozdicle <gizemozdicle@gmail.com>
2022-08-01 11:57:50 +0300

    Sample 1 Task

--- src/selenium/sample/Sample1Task.java
+++ src/selenium/sample/Sample1Task.java
@@ -3,6 +3,7 @@ package selenium.sample;
 
 import org.junit.Test;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 import java.io.File;
@@ -12,11 +13,15 @@ public class Sample1Task {
 
     @Test
     public void goToHomepage() throws Exception {
-//        TODO:
-//         define driver
+//         // define driver
+        WebDriver driver = new ChromeDriver();
 //         go to https://kristinek.github.io/site/index2.html
+        driver.get("https://kristinek.github.io/site/index2.html");
 //         get title of page
+        System.out.println(driver.getTitle());
 //         get URL of current page
+        System.out.println(driver.getCurrentUrl());
 //         close browser
+        driver.quit();
     }
 }
