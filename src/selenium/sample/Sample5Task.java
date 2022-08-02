bcff8e930dad5c030de749d87e77441b93361101
viktorija-korotkova <korotko.viktorija@gmail.com>
2022-08-02 10:29:37 +0300

    complete Sample5Task

--- src/selenium/sample/Sample5Task.java
+++ src/selenium/sample/Sample5Task.java
@@ -6,11 +6,13 @@ import org.junit.Test;
 import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 import java.io.File;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 public class Sample5Task {
     WebDriver driver;
@@ -36,21 +38,38 @@ public class Sample5Task {
     @Test
     public void goToAlertedPageViaButton() throws Exception {
 //         TODO:
-//        click on "To go to alerted page press Ok. Or stay here" button
+// click on "To go to alerted page press Ok. Or stay here" button
+        WebElement goToAlertedPageBtn= driver.findElement(By.className("w3-blue"));
+        String expectedPage ="https://kristinek.github.io/site/examples/alerted_page";
+        goToAlertedPageBtn.click();
+        Thread.sleep(1000);
 //        switch to alert
 //        click ok
+        driver.switchTo().alert().accept();
+        Thread.sleep(1000);
 //        switch to second alert
 //        verify alert text
+           assertEquals("Booooooooo!", driver.switchTo().alert().getText());
 //        click ok on second alert
+        driver.switchTo().alert().accept();
+        Thread.sleep(1000);
 //        verify that the correct page is opened
+        assertEquals(expectedPage, driver.getCurrentUrl());
     }
 
     @Test
     public void doNotGoToAlertedPageViaButton() throws Exception {
 //         TODO:
 //        click on "To go to alerted page press Ok. Or stay here" button
+          WebElement goToAlertedPageBtn= driver.findElement(By.className("w3-blue"));
+          goToAlertedPageBtn.click();
+          Thread.sleep(1000);
 //        switch to alert
+          driver.switchTo().alert().dismiss();
+          Thread.sleep(1000);
 //        click cancel
+          String textShown = "So you desided to say? Good!";
+          assertEquals(textShown, driver.findElement(By.id("textForAlerts")).getText());
 //        verify the text on page
     }
 }
