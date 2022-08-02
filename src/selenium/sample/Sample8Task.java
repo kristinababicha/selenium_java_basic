5663290cb9837c053100383353655ea3d265cae8
Janis Dzalbe <dzalbe.janis@accenture.com>
2022-08-02 16:29:43 +0300

    Sample8Task completed

--- src/selenium/sample/Sample8Task.java
+++ src/selenium/sample/Sample8Task.java
@@ -38,7 +38,12 @@ public class Sample8Task {
     public void styleChecks() throws Exception {
 //         TODO:
 //        check the background of top 2 sections
+        assertEquals("rgba(255, 221, 221, 1)",
+                driver.findElement(By.className("w3-pale-red")).getCssValue("background-color"));
+        assertEquals("rgba(255, 255, 204, 1)",
+                driver.findElement(By.className("w3-pale-yellow")).getCssValue("background-color"));
 //        rgba(255, 221, 221, 1);
 //        check h1 element font-size 64px
+        assertEquals("64px", driver.findElement(By.tagName("h1")).getCssValue("font-size"));
     }
 }
