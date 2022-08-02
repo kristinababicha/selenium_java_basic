adc5f584809ac15d05b71d2edaf233319c7907d8
Viktors Licovs <vlitsov@gmail.com>
2022-08-02 15:20:23 +0300

    sample 8 task

--- src/selenium/sample/Sample8Task.java
+++ src/selenium/sample/Sample8Task.java
@@ -37,14 +37,16 @@ public class Sample8Task {
     @Test
     public void styleChecks() throws Exception {
 //         TODO:
-//        check the background of top 2 sections
-//        rgba(255, 221, 221, 1) - red section;
+//        check the background of top 2 sections:
+//        1.rgba(255, 221, 221, 1) - red section;
         assertEquals("rgba(255, 221, 221, 1)", driver.findElement(By.className("w3-pale-red")).getCssValue("background-color"));
 
-//        rgba(255, 255, 204, 1) - yellow section;;
+//        2.rgba(255, 255, 204, 1) - yellow section;;
         assertEquals("rgba(255, 255, 204, 1)", driver.findElement(By.className("w3-pale-yellow")).getCssValue("background-color"));
 
 //        check h1 element font-size 64px
         assertEquals("64px", driver.findElement(By.className("w3-jumbo")).getCssValue("font-size"));
+
+        assertEquals("64px", driver.findElement(By.tagName("h1")).getCssValue("font-size"));
     }
 }
