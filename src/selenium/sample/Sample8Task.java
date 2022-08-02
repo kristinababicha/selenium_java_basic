847bfefa1f7556d946eceaefe8b90a887025b2ed
lienepurmale <96015681+lienepurmale@users.noreply.github.com>
2022-08-02 14:21:33 +0200

    Sample8Task

--- src/selenium/sample/Sample8Task.java
+++ src/selenium/sample/Sample8Task.java
@@ -7,6 +7,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.remote.RemoteWebElement;
 
 import java.io.File;
 
@@ -37,8 +38,18 @@ public class Sample8Task {
     @Test
     public void styleChecks() throws Exception {
 //         TODO:
+        WebElement header = driver.findElement(By.className("w3-pale-red"));
+        WebElement header2 = driver.findElement(By.className("w3-pale-yellow"));
+
 //        check the background of top 2 sections
 //        rgba(255, 221, 221, 1);
+        //rgba(255, 255, 204, 1);
+
+        assertEquals("rgba(255, 221, 221, 1)", header.getCssValue("background-color"));
+        assertEquals("rgba(255, 255, 204, 1)", header2.getCssValue("background-color"));
+
 //        check h1 element font-size 64px
+        WebElement header3 = driver.findElement(By.tagName("h1"));
+        assertEquals("64px", header3.getCssValue("font-size"));
     }
 }
