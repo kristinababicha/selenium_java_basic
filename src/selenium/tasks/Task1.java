73fea0963ede1ec3249cbe307216dff810cc60a6
viktorija-korotkova <korotko.viktorija@gmail.com>
2022-08-03 23:06:00 +0300

    partially complete individual Task1

--- src/selenium/tasks/Task1.java
+++ src/selenium/tasks/Task1.java
@@ -3,10 +3,18 @@ package selenium.tasks;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
-import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.remote.CapabilityType;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.io.File;
+import java.text.DecimalFormat;
+import java.util.concurrent.TimeUnit;
+
+import static org.junit.Assert.*;
 
 public class Task1 {
     WebDriver driver;
@@ -18,6 +26,7 @@ public class Task1 {
         System.setProperty("webdriver.chrome.driver", libWithDriversLocation + "chromedriver" + new selenium.ChangeToFileExtension().extension());
         driver = new ChromeDriver();
         driver.get("https://kristinek.github.io/site/tasks/enter_a_number");
+        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
     }
 
     @After
@@ -29,6 +38,13 @@ public class Task1 {
     public void errorOnText() {
 //        TODO
 //        enter a text instead of a number, check that correct error is seen
+        WebElement errorMessageElem;
+        WebElement inputField = driver.findElement(By.cssSelector("#numb"));
+        inputField.sendKeys("Test txt");
+        driver.findElement(By.cssSelector("button")).click();
+        errorMessageElem = driver.findElement(By.cssSelector("#ch1_error"));
+        assertTrue(errorMessageElem.isDisplayed());
+        assertEquals("Please enter a number",errorMessageElem.getText());
     }
 
     @Test
@@ -36,6 +52,13 @@ public class Task1 {
 //        BUG: if I enter number 49 or 42 no errors where seen
 //        TODO
 //        enter number which is too small (positive number below 50), check that correct error is seen
+        WebElement errorMessageElem;
+        WebElement inputField = driver.findElement(By.cssSelector("#numb"));
+        inputField.sendKeys("48");
+        driver.findElement(By.cssSelector("button")).click();
+        errorMessageElem = driver.findElement(By.cssSelector("#ch1_error"));
+        assertTrue(errorMessageElem.isDisplayed());
+        assertEquals("Number is too small",errorMessageElem.getText());
     }
 
     @Test
@@ -44,6 +67,13 @@ public class Task1 {
 //        BUG: if I enter number 666 no errors where seen
 //        TODO
 //        enter number which is too big (above 100), check that correct error is seen
+        WebElement errorMessageElem;
+        WebElement inputField = driver.findElement(By.cssSelector("#numb"));
+        inputField.sendKeys("128");
+        driver.findElement(By.cssSelector("button")).click();
+        errorMessageElem = driver.findElement(By.cssSelector("#ch1_error"));
+        assertTrue(errorMessageElem.isDisplayed());
+        assertEquals("Number is too big",errorMessageElem.getText());
     }
 
     @Test
