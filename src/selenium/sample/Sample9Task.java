1280cd46ec67c6e9e3020cb82dc4856fd7bbdd3c
Kristine Ringe <109912680+Christeen1@users.noreply.github.com>
2022-08-03 11:41:37 +0300

    Task 9

--- src/selenium/sample/Sample9Task.java
+++ src/selenium/sample/Sample9Task.java
@@ -12,8 +12,7 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 import java.io.File;
 import java.util.concurrent.TimeUnit;
 
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.*;
 
 public class Sample9Task {
     WebDriver driver;
@@ -34,47 +33,99 @@ public class Sample9Task {
     @Test
     public void loadGreenSleep() throws Exception {
 //         TODO:
+
 //         * 1) click on start loading green button
+        driver.findElement(By.id("start_green")).click();
 //         * 2) check that button does not appear,
+        assertFalse(driver.findElement(By.id("start_green")).isDisplayed());
 //         * but loading text is seen instead   "Loading green..."
+        assertTrue(driver.findElement(By.id("loading_green")).isDisplayed());
+        Thread.sleep(5000);
 //         * 3) check that both button
 //         * and loading text is not seen,
+        assertFalse(driver.findElement(By.id("start_green")).isDisplayed());
+        assertFalse(driver.findElement(By.id("loading_green")).isDisplayed());
 //         * success is seen instead "Green Loaded"
+        assertTrue(driver.findElement(By.id("finish_green")).isDisplayed());
     }
 
     @Test
     public void loadGreenImplicit() throws Exception {
 //         TODO:
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
 //         * 1) click on start loading green button
+        driver.findElement(By.id("start_green")).click();
 //         * 2) check that button does not appear,
+        assertFalse(driver.findElement(By.id("start_green")).isDisplayed());
 //         * but loading text is seen instead   "Loading green..."
-//         * 3) check that both button
+        assertTrue(driver.findElement(By.id("loading_green")).isDisplayed());
+
+        //    * 3) check that both button
 //         * and loading text is not seen,
 //         * success is seen instead "Green Loaded"
+//         * success is seen instead "Green Loaded"
+        assertTrue(driver.findElement(By.id("finish_green")).isDisplayed()); //because the element is not exist yet
+        assertFalse(driver.findElement(By.id("start_green")).isDisplayed());
+        assertFalse(driver.findElement(By.id("loading_green")).isDisplayed());
+
     }
 
     @Test
     public void loadGreenExplicitWait() throws Exception {
 //         TODO:
+        WebDriverWait wait = (WebDriverWait) new WebDriverWait(driver,10);
 //         * 1) click on start loading green button
+        driver.findElement(By.id("start_green")).click();
 //         * 2) check that button does not appear,
+        assertFalse(driver.findElement(By.id("start_green")).isDisplayed());
 //         * but loading text is seen instead   "Loading green..."
+        assertTrue(driver.findElement(By.id("loading_green")).isDisplayed());
 //         * 3) check that both button
 //         * and loading text is not seen,
 //         * success is seen instead "Green Loaded"
+wait.until(ExpectedConditions.presenceOfElementLocated(By.id("finish_green")));
+        assertFalse(driver.findElement(By.id("start_green")).isDisplayed());
+        assertFalse(driver.findElement(By.id("loading_green")).isDisplayed());
+        assertTrue(driver.findElement(By.id("finish_green")).isDisplayed());
     }
 
     @Test
     public void loadGreenAndBlueBonus() {
         /* TODO:
          * 0) wait until button to load green and blue appears
-         * 1) click on start loading green and blue button
-         * 2) check that button does not appear, but loading text is seen instead for green
+          * 1) click on start loading green and blue button
+        * 2) check that button does not appear, but loading text is seen instead for green
          * 3) check that button does not appear, but loading text is seen instead for green and blue
          * 4) check that button and loading green does not appear,
          * 		but loading text is seen instead for blue and success for green is seen
          * 5) check that both button and loading text is not seen, success is seen instead
          */
+        WebDriverWait wait = new WebDriverWait(driver,10);
+wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("start_green_and_blue")));
+        driver.findElement(By.id("start_green_and_blue")).click();
+
+        assertFalse(driver.findElement(By.id("start_green_and_blue")).isDisplayed());
+        assertTrue(driver.findElement(By.id("loading_green_without_blue")).isDisplayed());
+
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.id("loading_green_with_blue")));
+        assertFalse(driver.findElement(By.id("start_green_and_blue")).isDisplayed());
+        assertTrue(driver.findElement(By.id("loading_green_without_blue")).isDisplayed());
+        assertTrue(driver.findElement(By.id("loading_green_with_blue")).isDisplayed());
+
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.id("loading_blue_without_green")));
+        assertFalse(driver.findElement(By.id("start_green_and_blue")).isDisplayed());
+        assertFalse(driver.findElement(By.id("loading_green_without_blue")).isDisplayed());
+        assertTrue(driver.findElement(By.id("loading_green_with_blue")).isDisplayed());
+        assertTrue(driver.findElement(By.id("loading_blue_without_green")).isDisplayed());
+
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.id("finish_green_and_blue")));
+        assertFalse(driver.findElement(By.id("start_green_and_blue")).isDisplayed());
+        assertFalse(driver.findElement(By.id("loading_green_without_blue")).isDisplayed());
+        assertFalse(driver.findElement(By.id("loading_green_with_blue")).isDisplayed());
+        assertFalse(driver.findElement(By.id("loading_blue_without_green")).isDisplayed());
+        assertTrue(driver.findElement(By.id("finish_green_and_blue")).isDisplayed());
+
     }
 
 }
