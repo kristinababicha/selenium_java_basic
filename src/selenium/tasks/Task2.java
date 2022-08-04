ede43afcd0611a44b79b1c0df5e2a911c27ee324
ahmed <ahmed2050.mahmoud@gmail.com>
2022-08-04 15:01:12 +0200

    Task 2

--- src/selenium/tasks/Task2.java
+++ src/selenium/tasks/Task2.java
@@ -1,12 +1,19 @@
 package selenium.tasks;
 
 import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
 
 import java.io.File;
+
+import static com.sun.xml.internal.fastinfoset.stax.events.Util.isEmptyString;
+import static org.junit.Assert.*;
 
 public class Task2 {
     WebDriver driver;
@@ -28,54 +35,163 @@ public class Task2 {
     public void initialFeedbackPage() throws Exception {
 //         TODO:
 //         check that all field are empty and no ticks are clicked
+        WebElement name =driver.findElement(By.id("fb_name"));
+        assertEquals("",name.getText());
+        WebElement age =driver.findElement(By.id("fb_age"));
+        assertEquals("",name.getText());
+        //For language  none of the checkboxes are ticked
+        assertFalse(driver.findElement(By.xpath("//*[@id=\"lang_check\"]/input[1]")).isSelected());
+        assertFalse(driver.findElement(By.xpath("//*[@id=\"lang_check\"]/input[2]")).isSelected());
+        assertFalse(driver.findElement(By.xpath("//*[@id=\"lang_check\"]/input[3]")).isSelected());
+        assertFalse(driver.findElement(By.xpath("//*[@id=\"lang_check\"]/input[4]")).isSelected());
+        //For Grnre  none of the radiobutton (male or female only are ticked)
+        //Dont Know disabled
+
+        assertFalse(driver.findElement(By.xpath("//*[@id=\"fb_form\"]/form/div[4]/input[1]")).isSelected());
+        assertFalse(driver.findElement(By.xpath("//*[@id=\"fb_form\"]/form/div[4]/input[2]")).isSelected());
+
+//for Gender
 //         "Don't know" is selected in "Genre"
+        assertTrue(driver.findElement(By.xpath("//*[@id=\"fb_form\"]/form/div[4]/input[3]")).isSelected());
 //         "Choose your option" in "How do you like us?"
+        assertEquals("Choose your option",
+                driver.findElement(By.xpath("//*[@id=\"like_us\"]/option[1]")).getText());
 //         check that the button send is blue with white letters
+        assertEquals("rgba(33, 150, 243, 1)",
+                driver.findElement(By.xpath("//*[@id=\"fb_form\"]/form/button")).getCssValue("background-color"));
+        assertEquals("rgba(255, 255, 255, 1)",
+                driver.findElement(By.xpath("//*[@id=\"fb_form\"]/form/button")).getCssValue("color"));
     }
 
     @Test
     public void emptyFeedbackPage() throws Exception {
 //         TODO:
 //         click "Send" without entering any data
+        driver.findElement(By.xpath("//*[@id=\"fb_form\"]/form/button")).click();
+        //Thread.sleep(2000);
 //         check fields are empty or "null"
+      // assertTrue(driver.findElement(By.id("name")).getText().isEmpty());
+        assertEquals("",driver.findElement(By.id("name")).getText());
+        assertEquals("",driver.findElement(By.id("age")).getText());
+        assertEquals("",driver.findElement(By.id("language")).getText());
+        assertEquals("null",driver.findElement(By.id("gender")).getText());
+        assertEquals("null",driver.findElement(By.id("option")).getText());
+        assertEquals("",driver.findElement(By.id("comment")).getText());
+
 //         check button colors
 //         (green with white letter and red with white letters)
+        assertEquals("rgba(76, 175, 80, 1)"
+                ,driver.findElement(By.className("w3-green")).getCssValue("background-color"));
+        assertEquals("rgba(255, 255, 255, 1)",
+                driver.findElement(By.className("w3-green")).getCssValue("color"));
+
+        assertEquals("rgba(244, 67, 54, 1)"
+                ,driver.findElement(By.className("w3-red")).getCssValue("background-color"));
+        assertEquals("rgba(255, 255, 255, 1)",
+                driver.findElement(By.className("w3-red")).getCssValue("color"));
     }
+
 
     @Test
     public void notEmptyFeedbackPage() throws Exception {
 //         TODO:
 //         fill the whole form, click "Send"
-//         check fields are filled correctly
+        WebElement name = driver.findElement(By.name("name"));
+        name.sendKeys("Adam");
+        WebElement age = driver.findElement(By.name("age"));
+        age.sendKeys("20");
+        driver.findElement(By.xpath("//*[@id=\"lang_check\"]/input[1]")).click();
+        driver.findElement(By.xpath("//*[@id=\"fb_form\"]/form/div[4]/input[1]")).click();
+
+        Select dropdown = new Select(driver.findElement(By.id("like_us")));
+        dropdown.selectByVisibleText("Good");
+        WebElement comment = driver.findElement(By.name("comment"));
+        comment.sendKeys("Test For Test ");
+        driver.findElement(By.xpath("//*[@id=\"fb_form\"]/form/button")).click();
+        Thread.sleep(1000);
+        //         check fields are filled correctly
+        assertEquals("Adam",driver.findElement(By.id("name")).getText());
+        assertEquals("20",driver.findElement(By.id("age")).getText());
+        assertEquals("English",driver.findElement(By.id("language")).getText());
+        assertEquals("male",driver.findElement(By.id("gender")).getText());
+        assertEquals("Good",driver.findElement(By.id("option")).getText());
+        assertEquals("Test For Test",driver.findElement(By.id("comment")).getText());
+
 //         check button colors
 //         (green with white letter and red with white letters)
+        assertEquals("rgba(76, 175, 80, 1)"
+                ,driver.findElement(By.className("w3-green")).getCssValue("background-color"));
+        assertEquals("rgba(255, 255, 255, 1)",
+                driver.findElement(By.className("w3-green")).getCssValue("color"));
+
+        assertEquals("rgba(244, 67, 54, 1)"
+                ,driver.findElement(By.className("w3-red")).getCssValue("background-color"));
+        assertEquals("rgba(255, 255, 255, 1)",
+                driver.findElement(By.className("w3-red")).getCssValue("color"));
+
+
+
+
     }
 
     @Test
     public void yesOnWithNameFeedbackPage() throws Exception {
 //         TODO:
 //         enter only name
+        WebElement name = driver.findElement(By.name("name"));
+        name.sendKeys("Adam");
 //         click "Send"
+        driver.findElement(By.xpath("//*[@id=\"fb_form\"]/form/button")).click();
 //         click "Yes"
+        driver.findElement(By.xpath("//*[@id=\"fb_thx\"]/div/div[2]/button[1]")).click();
 //         check message text: "Thank you, NAME, for your feedback!"
+        assertEquals("Thank you, Adam, for your feedback!",driver.findElement(By.id("message")).getText());
 //         color of text is white with green on the background
+        assertEquals("rgba(76, 175, 80, 1)"
+                ,driver.findElement(By.className("w3-green")).getCssValue("background-color"));
+        assertEquals("rgba(255, 255, 255, 1)",
+                driver.findElement(By.className("w3-green")).getCssValue("color"));
     }
 
     @Test
     public void yesOnWithoutNameFeedbackPage() throws Exception {
 //         TODO:
 //         click "Send" (without entering anything
+        driver.findElement(By.className("w3-blue")).click();
 //         click "Yes"
+        driver.findElement(By.className("w3-green")).click();
 //         check message text: "Thank you for your feedback!"
+        assertEquals("Thank you for your feedback!",driver.findElement(By.id("message")).getText());
 //         color of text is white with green on the background
+        assertEquals("rgba(76, 175, 80, 1)"
+                ,driver.findElement(By.className("w3-green")).getCssValue("background-color"));
+        assertEquals("rgba(255, 255, 255, 1)",
+                driver.findElement(By.className("w3-green")).getCssValue("color"));
+
     }
 
     @Test
     public void noOnFeedbackPage() throws Exception {
 //         TODO:
 //         fill the whole form
+        WebElement name = driver.findElement(By.name("name"));
+        name.sendKeys("Adam");
+        WebElement age = driver.findElement(By.name("age"));
+        age.sendKeys("20");
+        driver.findElement(By.xpath("//*[@id=\"lang_check\"]/input[1]")).click();
+        driver.findElement(By.xpath("//*[@id=\"fb_form\"]/form/div[4]/input[1]")).click();
+
+        Select dropdown = new Select(driver.findElement(By.id("like_us")));
+        dropdown.selectByVisibleText("Good");
+        WebElement comment = driver.findElement(By.name("comment"));
+        comment.sendKeys("Test For Test ");
+
 //         click "Send"
+        driver.findElement(By.className("w3-blue")).click();
+        Thread.sleep(1000);
 //         click "No"
+        driver.findElement(By.className("w3-red")).click();
+        Thread.sleep(1000);
 //         check fields are filled correctly
     }
 }
